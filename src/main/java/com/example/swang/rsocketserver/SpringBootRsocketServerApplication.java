65d62748a1f0a73bfc0cfd1208e413c0540a9b5b
ming.wang4 <ming.wang4@envision-digital.com>
2022-12-27 21:09:55 +0800

    init

---
+++ src/main/java/com/example/swang/rsocketserver/SpringBootRsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.swang.rsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRsocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRsocketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swang/rsocketserver/SpringBootRsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swang.rsocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRsocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
